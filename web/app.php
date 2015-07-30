5360c1902d54c67e8e5ef5d2a15c5252ec2c870a
Melisa Cecilia <melisa.cecilia.mail@gmail.com>
2015-07-30 23:17:21 +0700

    test

--- web/app.php
+++ web/app.php
@@ -1,5 +1,5 @@
 <?php
-
+echo("Hello World!");
 error_reporting(E_ALL);
 define('FACEBOOK_SDK_V4_SRC_DIR', '/FacebookSDK/src/Facebook/');
 require __DIR__ . '/FacebookSDK/autoload.php';
@@ -11,21 +11,14 @@ $session = new FacebookSession('1018969218133716|rEQnvLNN_cNFT5ZrtftiXh9XgY8');
 $request = new FacebookRequest(
   $session,
   'POST',
-  '/1018969218133716/subscriptions',
+  '/149135891772134/subscriptions',
   array(
     'object' => 'page',
-    'callback_url' => 'https://snugglepuff.herokuapp.com/callback.phpv',
+    'callback_url' => 'http://requestb.in/1fqhrea1',
     'fields' => 'conversations',
     'verify_token' => 'test',
   )
 );
 $response = $request->execute();
-
-<div
-  class="fb-like"
-  data-share="true"
-  data-width="450"
-  data-show-faces="true">
-</div>
 
 ?>

4904384bf71c4ba647a5fba89a36d1affe151ee0
Melisa Cecilia <melisa.cecilia.mail@gmail.com>
2015-07-30 22:28:58 +0700

    add application token

--- web/app.php
+++ web/app.php
@@ -6,16 +6,16 @@ require __DIR__ . '/FacebookSDK/autoload.php';
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
 
-$session = new FacebookSession('<APP_ACCESS_TOKEN>');
+$session = new FacebookSession('1018969218133716|rEQnvLNN_cNFT5ZrtftiXh9XgY8');
 $request = new FacebookRequest(
   $session,
   'POST',
-  '/<APP_ID>/subscriptions',
+  '/1018969218133716/subscriptions',
   array(
     'object' => 'page',
-    'callback_url' => '<YOUR_CALLBACK_URL>',
+    'callback_url' => 'https://snugglepuff.herokuapp.com/callback.phpv',
     'fields' => 'conversations',
-    'verify_token' => '<YOUR_VERIFY_TOKEN>',
+    'verify_token' => 'test',
   )
 );
 $response = $request->execute();
